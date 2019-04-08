972609270bcfd5e7f74ba3b9d3aee4cf95cf66d3
Ricardo <ricardonovak@yahoo.com.br>
2019-04-08 19:07:26 -0300

    -Arrumado bug no mapa do anel (ele ta uma merda ainda)

--- Assets/Assets/Gameplay/Scripts/Scenario/Motors/RingArenaMotor.cs
+++ Assets/Assets/Gameplay/Scripts/Scenario/Motors/RingArenaMotor.cs
@@ -6,33 +6,34 @@ namespace Assets.Gameplay.Scripts.Scenario.Motors {
     public class RingArenaMotor : ArenaMotor {
 
 
-        private float m_timeRotating, m_rotateTime = 10;
-        
+        public float RotateTime = 10;
         private Transform m_ring;
         private Quaternion m_targetRotation, m_lastRotation;
 
         private Vector3 m_lastEulerRotation;
+
+        private float m_timeRotating;
         public override void Setup(ArenaController controller, ArenaState state) {
-            
+            m_timeRotating = 0;
         }
 
         protected override void OnInitialize(ArenaController controller, ArenaState state) {
             m_ring = ((RingArenaState) state).Ring;
-            SetNextRotation(m_lastEulerRotation = new Vector3(180, 0, 0));
+            m_lastEulerRotation = m_ring.rotation.eulerAngles;
+            SetNextRotation(m_lastEulerRotation += new Vector3(180, 0, 0));
             
         }
 
         public override void Tick(ArenaController controller, ArenaState state) {
             m_timeRotating += Time.deltaTime;
-            if (m_timeRotating > m_rotateTime) {
+            if (m_timeRotating > RotateTime) {
                 m_timeRotating = 0;
-                SetNextRotation(m_lastEulerRotation = new Vector3(m_lastEulerRotation.x + 180 * Random.Range(-1, 1), 0, 0));
+                SetNextRotation(m_lastEulerRotation += new Vector3(m_lastEulerRotation.x + 180 * Random.Range(-1, 1),0,0));
             }
-            m_ring.rotation = Quaternion.Slerp(m_lastRotation, m_targetRotation, m_timeRotating / m_rotateTime);
+            m_ring.rotation = Quaternion.Slerp(m_lastRotation, m_targetRotation, m_timeRotating / RotateTime);
         }
 
         public override void ResetToDefault(ArenaController controller, ArenaState state) {
-            
         }
 
         private void SetNextRotation(Vector3 euler) {
